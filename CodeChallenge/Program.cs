04bfbf3fd4ee992f3ae6b4eef84292c5c4cc666b
jmondragon <jmondragon@hugeinc.com>
2016-07-07 13:44:00 -0500

    organizing code

--- CodeChallenge/Program.cs
+++ CodeChallenge/Program.cs
@@ -10,13 +10,19 @@ namespace CodeChallenge
     {
         static void Main(string[] args)
         {
+            //Staircase();
+
+            Console.ReadLine();
+        }
+
+        public static void Staircase()
+        {
             int n = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 1; i < n + 1; i++)
             {
                 Console.Write(string.Concat(Enumerable.Repeat(" ", n - i)) + string.Concat(Enumerable.Repeat("#", i)) + (i < n ? "\n" : ""));
             }
-            Console.ReadLine();
         }
     }
 }
